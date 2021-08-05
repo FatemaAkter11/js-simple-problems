d3e7d7cb4d0318b57feb44036e0726af49af1f8d
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-06 00:50:28 +0600

    Largest element of an array

---
+++ largest-element.js
@@ -0,0 +1,17 @@
+// Find the largest element of an array
+
+function largestElement(numbers) {
+    // let largest = 0;
+    let largest = numbers[0];
+    for (let i = 0; i < numbers.length; i++) {
+        const element = numbers[i];
+        if (element > largest) {
+            largest = element;
+        }
+    }
+    return largest;
+}
+
+//const largestAges = largestElement([35, 12, 65, 90, 4, 19]);
+const largestAges = largestElement([-35, -2, -13, -10, -4, -19]);
+console.log('The Oldest Man', largestAges);
