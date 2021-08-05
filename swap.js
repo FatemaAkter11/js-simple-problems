20686993016f32f15911a7cc839ddcbe50e43f15
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-05 23:21:40 +0600

    swap variable, swap without temp, destructing

---
+++ swap.js
@@ -0,0 +1,19 @@
+//swap variable
+
+var firstNumber = 7;
+var seceondNumber = 8;
+//console.log(firstNumber, seceondNumber); //output = 7 8
+
+//swap with temp
+
+// var firstNumber = 7;
+// var seceondNumber = 8;
+// var temp = firstNumber;
+// firstNumber = seceondNumber;
+// seceondNumber = temp;
+//console.log(firstNumber, seceondNumber); // output = 8 7
+
+//destructing
+
+[firstNumber, seceondNumber] = [seceondNumber, firstNumber];
+console.log(firstNumber, seceondNumber);
